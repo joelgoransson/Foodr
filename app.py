007d3c6b9b90976f79c4a934be915d35f241a976
Joel Göransson <kallamackor@gmail.com>
2017-02-02 15:43:28 +0100

    Add upload feature

--- app.py
+++ app.py
@@ -1,6 +1,40 @@
-from flask import Flask, render_template, abort
+from flask import Flask, request, redirect, url_for, render_template, abort
+import os
+import uuid
 app = Flask("Foodr")
-visits = 0
+
+base = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(base, 'static', 'uploads')
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'png'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/picture/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+        	filename, file_extension = os.path.splitext(file.filename)
+        	file.save(os.path.join(app.config['UPLOAD_FOLDER'], str(uuid.uuid4()) + file_extension))
+        	return redirect(url_for('upload_file', filename=filename))
+    return render_template("upload.html")
+
+
+def user_is_logged_in():
+	return True
 
 def user_exists(user_id):
 	try:
@@ -9,6 +43,9 @@ def user_exists(user_id):
 	except:
 		return False
 
+def get_user_information(user_id):
+	return {"name": "Joel", "posts": 0, "diners": 0}
+
 def picture_exists(picture_id):
 	try:
 		picture_id = int(picture_id)
@@ -16,11 +53,10 @@ def picture_exists(picture_id):
 	except ValueError:
 		return False
 
+
 @app.route("/")
 def home():
-	global visits
-	visits += 1
-	return render_template('home.html', visits=visits, username="Joel")
+	return render_template('home.html')
 
 @app.route('/about/')
 def about():
@@ -33,11 +69,11 @@ def picture(picture_id):
 	else:
 		abort(404)
 
-
 @app.route('/user/<user_id>')
 def user(user_id):
 	if user_exists(user_id):
-		return render_template('user.html', user_id=user_id, username = user_id)
+		user_information = get_user_information(user_id)
+		return render_template('user.html', **user_information)
 	else:
 		abort(404)
 
