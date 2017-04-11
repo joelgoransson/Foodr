a86e112e5bf950709f2f21d5f4a373db9e86ccbb
Joel Göransson <kallamackor@gmail.com>
2017-04-11 08:34:53 +0100

    add database connect

--- models.py
+++ models.py
@@ -1,5 +1,14 @@
-from peewee import *
-db = PostgresqlDatabase('webapp')
+import os
+from peewee import PostgresqlDatabase, Model, CharField
+from playhouse.db_url import connect
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+if DATABASE_URL:
+    db = connect(DATABASE_URL)
+else:
+    DATABASE = 'webapp'
+    db = PostgresqlDatabase(DATABASE)
+
 class Guest(Model):
 	name = CharField()
 	email = CharField()
